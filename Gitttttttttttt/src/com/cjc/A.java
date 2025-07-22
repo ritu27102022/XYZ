5340dac541d5c1069fd9ea6d93ef39dee02438e2
LENOVO <LENOVO@DESKTOP-NMF9K3U>
2025-07-22 14:42:32 +0530

    A class

---
+++ Gitttttttttttt/src/com/cjc/A.java
@@ -0,0 +1,34 @@
+package com.cjc;
+
+public class A 
+{
+	public void add()
+	{
+		System.out.println("This is addition method");
+	}
+	public void mul()
+	{
+		System.out.println("This is multiplication method");
+	}
+	public void div()
+	{
+		System.out.println("This is division method");
+	}
+
+	public void sub()
+	{
+		System.out.println("This is substraction method");
+	}
+	
+	
+
+
+	
+ public static void main(String[] args) 
+ {
+	 System.out.println("This is main method");
+	 
+	
+}
+
+}
